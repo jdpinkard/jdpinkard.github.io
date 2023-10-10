6875dc9c516cf3e219a2649282f45a77afef44a7
Justin Pinkard <136210811+jdpinkard@users.noreply.github.com>
2023-10-09 23:03:05 -0500

    Update treasurehunt.js

--- treasurehunt.js
+++ treasurehunt.js
@@ -1,9 +1,8 @@
-var crown = Math.floor(Math.random()*9)
-var bomb1 = Math.floor(Math.random()*9)
-var bomb2 = Math.floor(Math.random()*9)
-var clickCounter = 3
-var bombFound = false
-var crownFound = false
+var crown = Math.floor(Math.random()*25)
+var bomb1 = Math.floor(Math.random()*25)
+var bomb2 = Math.floor(Math.random()*25)
+var clickCounter = 15
+
 function clickCounterPopUp() {
   var popup = document.getElementById("clicksPopup");
   popup.classList.toggle("show");
@@ -16,33 +15,32 @@ function bombPopUp() {
   var popup = document.getElementById("bombsPopup");
   popup.classList.toggle("show");
 }
+
 const treasure = (location) => {
-  if (bombFound === true) {
-    bombPopUp()
-  } else if (crownFound === true) {
-    crownPopUp()
-  } else if (clickCounter === 0) {
-    clickCounterPopUp()
+  if (clickCounter === 0) {
   } else {
     clickCounter --;
     if(location === crown){
       document.getElementById(location).innerHTML = "👑"
       crownPopUp()
-      crownFound = true
+      clickCounter = 0
     } else if(location === bomb1){
       document.getElementById(location).innerHTML = "💣"
       bombPopUp()
-      bombFound = true
+      clickCounter = 0
     } else if(location === bomb2){
       document.getElementById(location).innerHTML = "💣"
       bombPopUp()
-      bombFound = true
+      clickCounter = 0
     } else {
-      document.getElementById(location).innerHTML = "&#x1f332"
-      if (clickCounter === 0) {
+      document.getElementById(location).innerHTML = "🌴"
+        if (clickCounter === 0) {
         clickCounterPopUp()
       }
     }
+  }
+  document.getElementById("clicks").textContent = "Clicks left: " + clickCounter
+}
   }
   document.getElementById("clicks").textContent = "Clicks left: " + clickCounter
 }
